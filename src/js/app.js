d85a722175fbe0b233c112df77a5e86a6939c384
Viktoria Tretyak <vika.tretyak.95.95@mail.ru>
2021-03-04 10:54:37 +0800

    add domain.js game.js app.js

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+//console.log('app worked');
+  
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
